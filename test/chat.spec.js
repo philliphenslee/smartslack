d883d0694edb6990abd40945542a54067502ad35
Phillip Henslee <ph2@ph2.us>
2019-07-29 22:28:06 -0500

    test unit change

--- test/chat.spec.js
+++ test/chat.spec.js
@@ -188,21 +188,10 @@ describe('chat', function () {
             chat.updateMessage('string', 'string', 'string', function (err, result) {
                 expect(result).to.be.an('object');
                 expect(result.ok).to.equal(true);
-            });
-            done();
-        });
-        it('should return an api error response', function (done) {
-            var scope = nock('https://slack.com')
-                .post('/api/chat.update')
-                .reply(200, {
-                    ok: false,
-                    error: 'channel_not_found'
-                });
-            chat.updateMessage('string', 'string','string', function (err, result) {
-                expect(err).to.be.an('error');
-                expect(err.message).to.equal('channel_not_found');
                 done();
             });
+
         });
+
     });
 });
